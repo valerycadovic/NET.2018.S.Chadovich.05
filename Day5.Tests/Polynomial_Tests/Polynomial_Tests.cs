18a9bf634d6ea35f28bb1bf233b0b365096cded4
Valery Čadovič <valerachad03@gmail.com>
2018-07-08 17:58:32 +0300

    I'll do StyleCop in a few minutes!!

---
+++ Day5.Tests/Polynomial_Tests/Polynomial_Tests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Day5.ImmutablePolynomial;
+
+namespace Day5.Tests.Polynomial_Tests
+{
+    [TestFixture]
+    public class Polynomial_Tests
+    {
+        [TestCase()]
+        public void Ctor_Throws_FormatException_When_All_Zeros(params double[] values) =>
+            Assert.Throws<ArgumentException>(() => new Polynomial(values));
+
+        [TestCase(new double[] { 1d, 2d, 3d }, new double[] { 1d, 2d, 3d }, ExpectedResult = true)]
+        [TestCase(new double[] { 0d, 1d, 2d, 3d }, new double[] { 1d, 2d, 3d }, ExpectedResult = true)]
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, ExpectedResult = false)]
+        [TestCase(new double[] { 1d, 2d }, new double[] { 2d, 3d }, ExpectedResult = false)]
+        [TestCase(new double[] { 1.1999999999, -5.89999999999 }, new double[] { 1.2, -5.9 }, ExpectedResult = true)]
+        public bool Operator_Equals_Is_Correct(double[] l, double[] r)
+        {
+            Polynomial lhs = new Polynomial(l);
+            Polynomial rhs = new Polynomial(r);
+
+            return lhs == rhs;
+        }
+
+        [Test]
+        public void Operator_Equals_Is_Correct_With_Refs()
+        {
+            Polynomial a = new Polynomial(3);
+            Polynomial b = a;
+
+            Assert.IsTrue(a == b);
+        }
+
+        [Test]
+        public void Operator_Equals_Is_Correct_With_One_Null()
+        {
+            Polynomial a = new Polynomial(3);
+            Polynomial b = null;
+
+            Assert.IsFalse(a == b);
+        }
+
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, new double[] { 1d, 3d, 5d })]
+        [TestCase(new double[] { 15.5, -27.1, 0.0, 0.0, 345.223 }, new double[] { 35, 0.0, 1, 56 }, new double[] { 15.5, 7.9, 0.0, 1.0, 401.223 })]
+        public void Operator_Plus_Sums(double[] l, double[] r, double[] expectedResult)
+        {
+            Polynomial lhs = new Polynomial(l);
+            Polynomial rhs = new Polynomial(r);
+            Polynomial expected = new Polynomial(expectedResult);
+
+            Polynomial result = lhs + rhs;
+
+            Assert.IsTrue(result == expected);
+        }
+
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, new double[] { -1d, -1d, -1d })]
+        [TestCase(
+            new double[] { 15.5, -27.1, 0.0, 0.0, 345.223 },
+            new double[] { 35, 0.0, 1, 56 },
+            new double[] { 15.5, -62.1, 0.0, -1.0, 289.223 })]
+        public void Operator_Minus_Takes_Away(double[] l, double[] r, double[] expectedResult)
+        {
+            Polynomial lhs = new Polynomial(l);
+            Polynomial rhs = new Polynomial(r);
+            Polynomial expected = new Polynomial(expectedResult);
+
+            Polynomial result = lhs - rhs;
+
+            Assert.IsTrue(result == expected);
+        }
+
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, new double[] { 1d, 4d, 7d, 6d })]
+        [TestCase(
+            new double[] { 15.5, -27.1, 0.0, 0.0, 345.223 },
+            new double[] { 35, 0.0, 1, 56 },
+            new double[] { 542.5, -948.5, 15.5, 840.9, 10565.205, 0.0, 345.223, 19332.488 })]
+        public void Operator_Multiply_With_Polynomial(double[] l, double[] r, double[] expectedResult)
+        {
+            Polynomial lhs = new Polynomial(l);
+            Polynomial rhs = new Polynomial(r);
+            Polynomial expected = new Polynomial(expectedResult);
+
+            Polynomial result = lhs * rhs;
+
+            Assert.IsTrue(result == expected);
+        }
+
+
+        [TestCase(new double[] { 1, 2, 3 }, 2, new double[] { 2, 4, 6 })]
+        [TestCase(
+            new double[] { 15.5, -27.1, 0.0, 0.0, 345.223 },
+            5.2,
+            new double[] { 80.6, -140.92, 0.0, 0.0, 1795.1596 })]
+        public void Operator_Multiply_With_Number(double[] l, double r, double[] expectedResult)
+        {
+            Polynomial lhs = new Polynomial(l);
+            Polynomial expected = new Polynomial(expectedResult);
+
+            Polynomial result = lhs * r;
+
+            Assert.IsTrue(result == expected);
+        }
+
+        [TestCase(new double[] { 1d, 2d, 3d }, new double[] { 1d, 2d, 3d }, ExpectedResult = true)]
+        [TestCase(new double[] { 0d, 1d, 2d, 3d }, new double[] { 1d, 2d, 3d }, ExpectedResult = true)]
+        [TestCase(new double[] { 1d, 2d }, new double[] { 1d, 2d, 3d }, ExpectedResult = false)]
+        [TestCase(new double[] { 1d, 2d }, new double[] { 2d, 3d }, ExpectedResult = false)]
+        [TestCase(new double[] { 1.19999999999, -5.89999999999 }, new double[] { 1.2, -5.9 }, ExpectedResult = true)]
+        public bool Overrided_Equals_Is_Correct(double[] l, double[] r)
+        {
+            Polynomial lhs = new Polynomial(l);
+            Polynomial rhs = new Polynomial(r);
+
+            return lhs.Equals(rhs);
+        }
+
+        [Test]
+        public void Overrided_GetHashCode_Is_Correct()
+        {
+            Polynomial a = new Polynomial(new double[] { 1.1 });
+            Polynomial b = new Polynomial(new double[] { 1.1 });
+            Polynomial c = a;
+
+            HashSet<Polynomial> hs = new HashSet<Polynomial>(new Polynomial[] { a, b, c });
+
+            Assert.AreEqual(2, hs.Count);
+        }
+
+        [TestCase(new double[] { 1d, 2d, 3d }, ExpectedResult = "x^2 + 2x + 3")]
+        [TestCase(new double[] { 1d, 2d }, ExpectedResult = "x + 2")]
+        [TestCase(new double[] { 1.19999999999, -5.89999999999 }, ExpectedResult = "1,19999999999x - 5,89999999999")]
+        public string Overrided_ToString_Is_Correct(double[] l)
+        {
+            Polynomial lhs = new Polynomial(l);
+
+            return lhs.ToString();
+        }
+    }
+}
---
+++ Day5/Polynomial/Polynomial.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5.ImmutablePolynomial
+{
+    public sealed class Polynomial
+    {
+        private const double epsilon = 10e-10;
+        private readonly string stringRepresentation;
+
+        public Polynomial(params double[] coeffs)
+        {
+            if (coeffs.Length == 0)
+            {
+                throw new ArgumentException($"{coeffs} must have at least one argument");
+            }
+
+            int firstNonZero = FindFirstValue(coeffs);
+
+            if (firstNonZero == -1)
+            {
+                this.Coeffs = new double[] { 0.0 };
+                this.stringRepresentation = FormStringRepresentation();
+                return;
+            }
+
+            this.Power = coeffs.Length - firstNonZero - 1;
+
+            this.Coeffs = new double[this.Power + 1];
+            Array.Copy(coeffs, firstNonZero, this.Coeffs, 0, this.Power + 1);
+
+            this.stringRepresentation = FormStringRepresentation();
+        }
+
+        public double[] Coeffs { get; }
+
+        public int Power { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            switch (obj)
+            {
+                case null:
+                    return false;
+                case Polynomial p when p == this:
+                    return true;
+                default: return false;
+            }
+        }
+
+        public override int GetHashCode() => this.Coeffs.GetHashCode();
+
+        public override string ToString()
+        {
+            return this.stringRepresentation;
+        }
+
+        private string FormStringRepresentation()
+        {
+            if (this.Power == 0)
+            {
+                return this.Coeffs[0].ToString();
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(FormFirst());
+
+            if (this.Power != 1)
+            {
+                sb.Append(FormMiddle());
+            }
+
+            sb.Append(FormLast());
+
+            return sb.ToString();
+        }
+
+        private string FormFirst()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (!CompareDouble(this.Coeffs[0], 1))
+            {
+                sb.Append(this.Coeffs[0]);
+            }
+
+            sb.Append(this.Power > 1 ? $"x^{this.Power}" : "x");
+
+            return sb.ToString();
+        }
+
+        private string FormMiddle()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i < this.Coeffs.Length - 1; i++)
+            {
+                if (CompareDouble(this.Coeffs[i], 0.0))
+                {
+                    continue;
+                }
+
+                sb.Append(this.Coeffs[i] < 0 ? " - " : " + ");
+                sb.Append(FormTerm(i));
+            }
+
+            return sb.ToString();
+        }
+
+        private string FormLast()
+        {
+            int last = this.Coeffs.Length - 1;
+
+            if (CompareDouble(this.Coeffs[last], 0.0))
+            {
+                return string.Empty;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(this.Coeffs[last] < 0 ? " - " : " + ");
+            sb.Append(Math.Abs(this.Coeffs[last]));
+
+            return sb.ToString();
+        }
+
+        private string FormTerm(int index)
+        {
+            StringBuilder sb = new StringBuilder();
+            int currentPower = this.Power - index;
+
+            if (!CompareDouble(this.Coeffs[index], 1.0))
+            {
+                sb.Append(Math.Abs(this.Coeffs[index]));
+            }
+
+            sb.Append(currentPower == 1 ? "x" : $"x^{currentPower}");
+
+            return sb.ToString();
+        }
+
+        #region Operators overloads
+        public static bool operator ==(Polynomial lhs, Polynomial rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return true;
+            }
+
+            if (lhs is null || rhs is null)
+            {
+                return false;
+            }
+
+            return ValueEquals(lhs.Coeffs, rhs.Coeffs);
+        }
+
+        public static bool operator !=(Polynomial lhs, Polynomial rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public static Polynomial operator +(Polynomial lhs, Polynomial rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Sum(lhs.Coeffs, rhs.Coeffs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator +(Polynomial lhs, double[] rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Sum(lhs.Coeffs, rhs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator +(double[] lhs, Polynomial rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Sum(lhs, rhs.Coeffs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator -(Polynomial lhs, Polynomial rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Diff(lhs.Coeffs, rhs.Coeffs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator -(Polynomial lhs, double[] rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Diff(lhs.Coeffs, rhs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator -(double[] lhs, Polynomial rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Diff(lhs, rhs.Coeffs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator *(Polynomial lhs, Polynomial rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Mul(lhs.Coeffs, rhs.Coeffs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator *(Polynomial lhs, double rhs)
+        {
+            IsNull(lhs, nameof(lhs));
+
+            double[] result = Mul(lhs.Coeffs, rhs);
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator *(double lhs, Polynomial rhs)
+        {
+            IsNull(rhs, nameof(rhs));
+
+            double[] result = Mul(rhs.Coeffs, lhs);
+
+            return new Polynomial(result);
+        }
+        #endregion
+
+        #region Private operations with arrays
+        private static double[] Mul(double[] lhs, double rhs)
+        {
+            double[] result = new double[lhs.Length];
+
+            for (int i = 0; i < lhs.Length; i++)
+            {
+                result[i] = lhs[i] * rhs;
+            }
+
+            return result;
+        }
+
+        private static double[] Mul(double[] lhs, double[] rhs)
+        {
+            double[] result = new double[lhs.Length + rhs.Length - 1];
+
+            for (int i = 0; i < lhs.Length; i++)
+            {
+                if (Math.Abs(lhs[i]) > epsilon)
+                {
+                    for (int j = 0; j < rhs.Length; j++)
+                    {
+                        result[i + j] += lhs[i] * rhs[j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static bool ValueEquals(double[] lhs, double[] rhs)
+        {
+            if (rhs.Length != lhs.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < rhs.Length; i++)
+            {
+                if (!CompareDouble(lhs[i], rhs[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static double[] Sum(double[] lhs, double[] rhs)
+        {
+            var (longer, shorter) = FindLongerAndShorter(lhs, rhs);
+
+            int difference = CopyDifference(longer, shorter, out double[] result);
+
+            for (int i = difference; i < longer.Length; i++)
+            {
+                result[i] = longer[i] + shorter[i - difference];
+            }
+
+            return result;
+        }
+
+        private static double[] Diff(double[] lhs, double[] rhs)
+        {
+            var (longer, shorter) = FindLongerAndShorter(lhs, rhs);
+
+            if (longer == rhs)
+            {
+                longer = InverseValues(longer);
+                shorter = InverseValues(shorter);
+            }
+
+            int difference = CopyDifference(longer, shorter, out double[] result);
+
+            for (int i = difference; i < longer.Length; i++)
+            {
+                result[i] = longer[i] - shorter[i - difference];
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region Other private helpers
+        private static int CopyDifference(double[] longer, double[] shorter, out double[] result)
+        {
+
+            int size = longer.Length;
+            result = new double[size];
+
+            int difference = longer.Length - shorter.Length;
+            if (difference != 0)
+            {
+                Array.Copy(longer, result, difference);
+            }
+
+            return difference;
+        }
+
+        private static (double[] longer, double[] shorter) FindLongerAndShorter(double[] lhs, double[] rhs)
+        {
+            double[] shorter, longer;
+
+            if (lhs.Length > rhs.Length)
+            {
+                shorter = rhs;
+                longer = lhs;
+            }
+            else
+            {
+                shorter = lhs;
+                longer = rhs;
+            }
+
+            return (longer, shorter);
+        }
+
+        private int FindFirstValue(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!CompareDouble(array[i], 0.0))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private static double[] InverseValues(double[] array)
+        {
+            double[] result = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                result[i] = -1 * array[i];
+            }
+
+            return result;
+        }
+
+        private static bool CompareDouble(double a, double b) => Math.Abs(a - b) < epsilon;
+
+        private static void IsNull(object o, string name)
+        {
+            if (o == null)
+            {
+                throw new ArgumentNullException($"{name} is null");
+            }
+        }
+        #endregion
+    }
+}
